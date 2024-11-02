7acb84258a8c05987c2b12122d1510aca5e6b1ea
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-11-01 23:42:51 -0500

    First commit

---
+++ src/main/java/pe/edu/unfv/cursoarquitecturahexagonalauthentication/CursoArquitecturaHexagonalAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.cursoarquitecturahexagonalauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoArquitecturaHexagonalAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoArquitecturaHexagonalAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/unfv/cursoarquitecturahexagonalauthentication/CursoArquitecturaHexagonalAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.cursoarquitecturahexagonalauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoArquitecturaHexagonalAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
